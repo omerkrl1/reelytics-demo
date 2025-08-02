f6e11107b11be20bb2448b53f91499fd2871a391
omerkrl1 <krlomer17@gmail.com>
2025-08-03 00:45:09 +0300

    Create Dashboard.jsx

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <p>Dashboard içeriği burada olacak.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
